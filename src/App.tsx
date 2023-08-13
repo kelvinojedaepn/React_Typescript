0ec4800e129b422f21f7951aed0097c76aa95ea1
KelvinOjedaEpn <kelvin.ojeda@epn.edu.ec>
2023-08-13 00:41:23 -0500

    Mod: Until todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react"
+import React, {useState} from "react"
 
 import "./App.css"
 import {InputFeild} from "./components/InputFeild"
+import {Todo} from "./model"
+import TodoList from "./components/TodoList"
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("")
+
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (todo) {
+      // console.log(todo)
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
+      setTodo("")
+    }
+  }
+
   return (
     <div className="App">
       <span className="heading">Task</span>
-      <InputFeild />
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   )
 }
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,10 +1,35 @@
-import React from "react"
+import React, {useRef} from "react"
 import "./style.css"
-export const InputFeild = () => {
+
+interface Props {
+  todo: string
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleAdd: (e: React.FormEvent) => void
+}
+
+export const InputFeild: React.FC<Props> = ({
+  todo,
+  setTodo,
+  handleAdd,
+}: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null)
   return (
-    <form className="input">
-      <input type="text" placeholder="Enter a task" className="input__box" />
-      <button className="input__submit" type="submit">
+    <form
+      className="input"
+      onSubmit={e => {
+        handleAdd(e)
+        inputRef.current?.blur()
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        value={todo}
+        onChange={e => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+      />
+      <button className="input_submit" type="submit">
         Go
       </button>
     </form>
